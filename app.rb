fe0c4a1c6e8b5377e8526abbffddf9c08d07535c
Max DePriest <maxd924@gmail.com>
2015-05-29 15:31:55 -0500

    More stuff

--- app.rb
+++ app.rb
@@ -1,11 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
 
+require_relative './user.rb'
+
 get "/" do 
 	# @photo_array = []
 	# for (Photo.count) do |url|
 	# 	@photo_array << (Photo.find(url))
 	# end
+	@image = Photo.find(1)
+	@image_two = Photo.find(2)
 	
 	erb :index
 end	
