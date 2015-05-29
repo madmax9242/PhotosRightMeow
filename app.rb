5ab1fed2c1b793d9457b6a9a206a44d1b32613a5
Max DePriest <maxd924@gmail.com>
2015-05-29 15:06:16 -0500

    Trying to sort out table parsing

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do 
+	# @photo_array = []
+	# for (Photo.count) do |url|
+	# 	@photo_array << (Photo.find(url))
+	# end
+	
 	erb :index
 end	
 
